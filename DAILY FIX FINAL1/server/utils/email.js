0b1c4fd192a8bac58a88b08fce77c1268529fde2
Monicanatarajan <nadarajanmonica@gmail.com>
2026-04-05 08:29:56 +0530

    debug resend response

--- DAILY FIX FINAL1/server/utils/email.js
+++ DAILY FIX FINAL1/server/utils/email.js
@@ -5,12 +5,13 @@ console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
 
 const sendOTP = async (email, otp) => {
     try {
-        await resend.emails.send({
+        const response = await resend.emails.send({
             from: 'onboarding@resend.dev',
             to: email,
             subject: 'Your OTP Code',
             html: `<h2>Your OTP is: ${otp}</h2>`
         });
+        console.log("RESEND RESPONSE:", response);
         console.log("OTP sent successfully");
     } catch (error) {
         console.error("Error sending OTP:", error);
